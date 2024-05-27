845abe7cdbded9ef5bd0f22fad7089ae8daf9451
加藤嵩大 <takahiro@katoushiori-no-MacBook-Air.local>
2024-05-27 17:38:15 +0900

    TEST

--- predict.py
+++ predict.py
@@ -1,48 +1,46 @@
 import pandas as pd
 import sys
 import numpy as np
-import lightgbm
 
 from sklearn.multioutput import MultiOutputRegressor
+from sklearn import preprocessing
+#from sklearn.linear_model import 
 from sklearn.ensemble import RandomForestRegressor
-from sklearn.linear_model import LogisticRegression
-
-
 from sklearn.metrics import r2_score
 
 from sklearn.model_selection import train_test_split
+import math
 
+mm = preprocessing.MinMaxScaler()
 
 df = pd.read_csv(sys.argv[1])
 
-distance = int(sys.argv[2]) / 1000
-round_amount = int(sys.argv[3])
+distance = (int(sys.argv[2]))
+round_amount = (int(sys.argv[3]))
 weather  = sys.argv[4]
-max_kion = float(sys.argv[5])
-min_kion = float(sys.argv[6])
-race_point = float(sys.argv[7]) / 100
-s_value  = int(sys.argv[8])
-b_value  = int(sys.argv[9])
+max_kion = (float(sys.argv[5]))
+min_kion = (float(sys.argv[6]))
+race_point = (float(sys.argv[7]))
+s_value  = (int(sys.argv[8]))
+b_value  = (int(sys.argv[9]))
 food_type = sys.argv[10]
-nige_point = int(sys.argv[11])
-ken_point = int(sys.argv[12])
-sa_point  = int(sys.argv[13])
-ma_point  = int(sys.argv[14])
-first_point = int(sys.argv[15])
-second_point = int(sys.argv[16])
-third_point = int(sys.argv[17])
-takeoff_point = int(sys.argv[18])
+nige_point = (int(sys.argv[11]))
+ken_point = (int(sys.argv[12]))
+sa_point  = (int(sys.argv[13]))
+ma_point  = (int(sys.argv[14]))
+first_point = (int(sys.argv[15]))
+second_point = (int(sys.argv[16]))
+third_point =  (int(sys.argv[17]))
+takeoff_point = (int(sys.argv[18]))
 
-win_percent = float(sys.argv[19]) / 100 
-ninren_tairitsu = float(sys.argv[20]) / 100
-sanren_tairitsu = float(sys.argv[21]) / 100
-gear_percent = float(sys.argv[22]) / 100
+win_percent = (float(sys.argv[19]))
+ninren_tairitsu = (float(sys.argv[20]))
+sanren_tairitsu = (float(sys.argv[21]))
+gear_percent = (float(sys.argv[22]))
 
 X = df[["距離", "ラウンド数", "天気", "最高気温", "最低気温", "競走得点", "S", "B", "脚", "逃", "捲", "差", "マ", "1着", "2着", "3着", "着外", "勝率", "2連対率", "3連対率", "ギヤ倍率"]]
 y = df[["is_first", "is_second", "is_third", "is_takeoff"]]
 
-X["距離"] / 1000
-X["競走得点"] /= 100
 predict_data = None
 if weather == "晴":
     predict_data = [distance, 
@@ -64,9 +62,9 @@ if weather == "晴":
                     ninren_tairitsu,
                     sanren_tairitsu,
                     gear_percent,
-                    True,
-                    False,
-                    False]
+                    1,
+                    0,
+                    0]
 elif weather == "曇":
     predict_data = [distance, 
                     round_amount,
@@ -87,9 +85,9 @@ elif weather == "曇":
                     ninren_tairitsu,
                     sanren_tairitsu,
                     gear_percent,
-                    False,
-                    True,
-                    False]
+                    0,
+                    1,
+                    0]
 elif weather == "雨":
     predict_data = [distance, 
                     round_amount,
@@ -110,34 +108,36 @@ elif weather == "雨":
                     ninren_tairitsu,
                     sanren_tairitsu,
                     gear_percent,
-                    False,
-                    False,
-                    True]
+                    0,
+                    0,
+                    1]
 
 if food_type == "追":
-    predict_data.append(True)
-    predict_data.append(False)
-    predict_data.append(False)
+    predict_data.append(1)
+    predict_data.append(0)
+    predict_data.append(0)
 elif food_type == "逃":
-    predict_data.append(False)
-    predict_data.append(True)
-    predict_data.append(False)
+    predict_data.append(0)
+    predict_data.append(1)
+    predict_data.append(0)
 elif food_type == "両":
-    predict_data.append(False)
-    predict_data.append(False)
-    predict_data.append(True)
+    predict_data.append(0)
+    predict_data.append(0)
+    predict_data.append(1)
 
+reg = RandomForestRegressor(max_depth=1500)#RandomForestRegressor(max_depth=1000) #MultiOutputRegressor(LogisticRegression())
+
+X = pd.get_dummies(X, columns=["天気", "脚"])
 print(predict_data)
-reg = RandomForestRegressor(max_depth=1000) #MultiOutputRegressor(LogisticRegression())
-
-X = pd.get_dummies(X, columns=["天気", "脚"], dtype=bool)
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=0)
 
+X_train = mm.fit_transform(X_train)
 reg.fit(X_train, y_train)
 
-pred= reg.predict([predict_data])
-result = pred[0]
+predict_data = mm.fit_transform([predict_data])
+pred= reg.predict(predict_data)
+result = (pred[0])
 win_result = np.argmax(result[0:4]) + 1
 print(result)
 print("着順予想")
